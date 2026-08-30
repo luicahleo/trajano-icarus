149d2f1a5b4a1620ea0d3642c34c69bdfc744ec9
luis cahuana <luisrolandocahuanaleon@gmail.com>
2026-08-30 15:02:50 +0200

    fix(seed): quitar texto pegado que rompía la compilación de SemillaIdentidad

--- Icarus/src/Identity/Icarus.Identity.Infrastructure/SemillaIdentidad.cs
+++ Icarus/src/Identity/Icarus.Identity.Infrastructure/SemillaIdentidad.cs
@@ -12,7 +12,7 @@ public static class SemillaIdentidad
 {
     public static readonly Guid ClienteDemoId = new("11111111-1111-1111-1111-111111111111");
     public static readonly Guid TrabajadorDemoId = new("22222222-2222-2222-2222-222222222222");
-cual es l    public static readonly Guid ClienteC1Id = new("33333333-3333-3333-3333-333333333333");
+    public static readonly Guid ClienteC1Id = new("33333333-3333-3333-3333-333333333333");
     public static readonly Guid TrabajadorT1Id = new("44444444-4444-4444-4444-444444444444");
 
     public const string EmailAdmin = "admin@icarus.test";
